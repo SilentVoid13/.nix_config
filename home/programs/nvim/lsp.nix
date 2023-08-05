56fa5127e28fb67038f534ba02968d7cdc3dadca
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2023-08-05 21:18:17 +0200

    feat: adding copilot to nvim config

--- home/programs/nvim/lsp.nix
+++ home/programs/nvim/lsp.nix
@@ -96,10 +96,21 @@ in {
         };
         sources = [
           {name = "nvim_lsp";}
+          {name = "copilot";}
           {name = "luasnip";}
           {name = "buffer";}
         ];
         snippet.expand = "luasnip";
+      };
+
+      copilot-lua = {
+        enable = true;
+        panel.enabled = false;
+        suggestion.enabled = false;
+      };
+
+      copilot-cmp = {
+        enable = true;
       };
 
       luasnip = {

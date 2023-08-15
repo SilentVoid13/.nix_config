bacdf40f24624378e1bef9f8e86c1f7d8c9ef88f
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2023-08-15 18:54:17 +0200

    feat: adding tmux nix config

--- home/main.nix
+++ home/main.nix
@@ -8,27 +8,34 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
 
+    ## Common
     ./programs/common_terminal.nix
     ./programs/common_gui.nix
     ./programs/common_toolchain.nix
     ./programs/common_hacking.nix
+
+    ## GUI
     ./programs/1password.nix
-    ./programs/git
-    ./programs/brave.nix
+    #./programs/brave.nix
     ./programs/firefox.nix
-    ./programs/alacritty.nix
-    ./programs/foot.nix
-    ./programs/sway
-    ./programs/wofi.nix
-    ./programs/fuzzel.nix
-    ./programs/waybar.nix
     ./programs/binaryninja
-    ./programs/gdb.nix
     ./programs/mpv.nix
-    ./programs/zsh.nix
+
+    ##  Terminal
+    ./programs/git
+    #./programs/alacritty.nix
+    ./programs/foot.nix
     ./programs/nvim
+    ./programs/gdb.nix
+    ./programs/zsh.nix
+    ./programs/tmux.nix
     ./programs/nodejs.nix
 
+    ## OS
+    ./programs/sway
+    #./programs/wofi.nix
+    ./programs/fuzzel.nix
+    ./programs/waybar.nix
     ./services/dunst.nix
   ];
 }
---
+++ home/programs/tmux.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+in {
+    programs.tmux = {
+        enable = true;
+
+        keyMode = "vi";
+        prefix = "C-a";
+        baseIndex = 1;
+        escapeTime = 0;
+        terminal = "screen-256color";
+        shell = "${pkgs.zsh}/bin/zsh";
+        historyLimit = 5000;
+
+        # maybe
+        # set-window-option -g xterm-keys on
+
+        extraConfig = ''
+            bind -r ^ last-window
+            bind -r k select-pane -U
+            bind -r j select-pane -D
+            bind -r h select-pane -L
+            bind -r l select-pane -R
+
+            bind -T copy-mode-vi v send -X begin-selection
+            bind -T copy-mode-vi y send -X copy-selection-and-cancel
+            bind -T copy-mode-vi Escape send -X cancel
+
+            bind '"' split-window -c "#{pane_current_path}"
+            bind % split-window -h -c "#{pane_current_path}"
+            bind c new-window -c "#{pane_current_path}"
+    
+            set -g status-style 'bg=#333333 fg=#5eacd3'
+
+            set -g renumber-windows on
+        '';
+    };
+}

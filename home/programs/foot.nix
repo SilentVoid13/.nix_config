1620b83c3a53cbb82b5a4e4b7b4e1d783dba04ce
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-07-22 12:32:08 +0200

    feat(stylix): updating conf

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -3,23 +3,11 @@
     enable = true;
     settings = {
       main = {
-        # handled by stylix
-        #font = "terminus:size=12";
-        #font = "Iosevka Nerd Font Mono:size=12";
-        #font = "Iosevka:size=12";
-
+        font = "Iosevka Nerd Font Mono:size=14";
         selection-target = "clipboard";
       };
 
-
       colors = lib.mkForce {
-        alpha = 0.9;
-        background = "000000";
-        foreground = "fce8c3";
-      };
-      # handled by stylix
-      /*
-      colors = {
         alpha = 0.9;
         background = "000000";
         foreground = "fce8c3";
@@ -41,44 +29,7 @@
         bright5 = "ff5c8f";
         bright6 = "2be4d0";
         bright7 = "fce8c3";
-
-        # gruvbox-dark
-        #regular0 = "282828";
-        #regular1 = "cc241d";
-        #regular2 = "98971a";
-        #regular3 = "d79921";
-        #regular4 = "458588";
-        #regular5 = "b16286";
-        #regular6 = "689d6a";
-        #regular7 = "a89984";
-        #bright0 = "928374";
-        #bright1 = "fb4934";
-        #bright2 = "b8bb26";
-        #bright3 = "fabd2f";
-        #bright4 = "83a598";
-        #bright5 = "d3869b";
-        #bright6 = "8ec07c";
-        #bright7 = "ebdbb2";
-
-        # tokyonight-night
-        #regular0 = "15161E";
-        #regular1 = "f7768e";
-        #regular2 = "9ece6a";
-        #regular3 = "e0af68";
-        #regular4 = "7aa2f7";
-        #regular5 = "bb9af7";
-        #regular6 = "7dcfff";
-        #regular7 = "a9b1d6";
-        #bright0 = "414868";
-        #bright1 = "f7768e";
-        #bright2 = "9ece6a";
-        #bright3 = "e0af68";
-        #bright4 = "7aa2f7";
-        #bright5 = "bb9af7";
-        #bright6 = "7dcfff";
-        #bright7 = "c0caf5";
       };
-      */
     };
   };
 }
--- home/programs/stylix.nix
+++ home/programs/stylix.nix
@@ -11,28 +11,37 @@
     enable = true;
     #polarity = "dark";
     image = config.lib.stylix.pixel "base0A";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-
-    base16Scheme = {
-        base00 = "000000"; # ---- black
-        base01 = "202020"; # --- black
-        base02 = "202020"; # -- black
-        base03 = "404040"; # - black
-        base04 = "FFFFFF"; # dark white
-        base05 = "FFFFFF"; # white
-        base06 = "FFFFFF"; # middle white
-        base07 = "FFFFFF"; # bright white
-
-        base08 = "EF2F27"; # red
-        base09 = "FF5F00"; # orange
-        base0A = "FBB829"; # yellow
-        base0B = "519F50"; # green
-        base0C = "0AAEB3"; # cyan
-        base0D = "2C78BF"; # blue
-        base0E = "E02C6D"; # purple
-        base0F = "2BE4D0"; # brown
+    override = {
+      scheme = "custom-tokyo-night-dark";
+      base00 = "000000";
+      base04 = "FFFFFF"; # dark white
+      base05 = "FFFFFF"; # white
+      base06 = "FFFFFF"; # middle white
+      base07 = "FFFFFF"; # bright white
     };
+    #base16Scheme = {
+    #  base00 = "000000"; # ---- black
+    #  base01 = "202020"; # --- black
+    #  base02 = "202020"; # -- black
+    #  base03 = "404040"; # - black
+    #  base04 = "FFFFFF"; # dark white
+    #  base05 = "FFFFFF"; # white
+    #  base06 = "FFFFFF"; # middle white
+    #  base07 = "FFFFFF"; # bright white
+
+    #  base08 = "EF2F27"; # red
+    #  base09 = "FF5F00"; # orange
+    #  base0A = "FBB829"; # yellow
+    #  base0B = "519F50"; # green
+    #  base0C = "0AAEB3"; # cyan
+    #  base0D = "2C78BF"; # blue
+    #  base0E = "E02C6D"; # purple
+    #  base0F = "2BE4D0"; # brown
+    #};
 
     cursor = {
       package = pkgs.bibata-cursors;
@@ -45,6 +54,11 @@
     };
     fonts.sizes = {
       terminal = 14;
+    };
+
+    targets = {
+      nixvim.enable = false;
+      foot.enable = false;
     };
   };
 }

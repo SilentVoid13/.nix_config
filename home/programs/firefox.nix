29277284026d4c898e140b1f8f3a3115ab8b736c
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-09-22 20:22:19 +0200

    feat(firefox): using ExtensionSettings to auto-pin/enable extensions

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -18,6 +18,10 @@
       id = 0;
       isDefault = true;
       name = "0";
+      settings = {
+        # prevent extension disabling
+        "extensions.autoDisableScopes" = "0";
+      };
       arkenfox = {
         enable = true;
         # disable about:config warning
@@ -37,33 +41,88 @@
         default = "DuckDuckGo";
         force = true;
       };
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        onepassword-password-manager
-        ublock-origin
-        sponsorblock
-        cookie-autodelete
-        omnivore
-        web-scrobbler
-        #tridactyl
-        #clearurls
-        ublacklist
-        (buildFirefoxXpiAddon {
-          pname = "PwnFox";
-          version = "1.0.4";
-          addonId = "PwnFox@bi.tk";
-          url = "https://addons.mozilla.org/firefox/downloads/file/3996527/pwnfox-1.0.4.xpi";
-          sha256 = "a4771718dba038132d00c8a4f472eb777a9720af68faf65aa0579ee67cbb64cc";
-          meta = {};
-        })
-        (buildFirefoxXpiAddon {
-          pname = "Cookie-Editor";
-          version = "1.13.0";
-          addonId = "{c3c10168-4186-445c-9c5b-63f12b8e2c87}";
-          url = "https://addons.mozilla.org/firefox/downloads/file/4241002/cookie_editor-1.13.0.xpi";
-          sha256 = "3d6fd83a8343dfa5e4461d83c2856264fb74b36b1c165305168d013f4831dbb0";
-          meta = {};
-        })
-      ];
+      #extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      #  onepassword-password-manager
+      #  ublock-origin
+      #  sponsorblock
+      #  cookie-autodelete
+      #  omnivore
+      #  web-scrobbler
+      #  #tridactyl
+      #  #clearurls
+      #  ublacklist
+      #  (buildFirefoxXpiAddon {
+      #    pname = "PwnFox";
+      #    version = "1.0.4";
+      #    addonId = "PwnFox@bi.tk";
+      #    url = "https://addons.mozilla.org/firefox/downloads/file/3996527/pwnfox-1.0.4.xpi";
+      #    sha256 = "a4771718dba038132d00c8a4f472eb777a9720af68faf65aa0579ee67cbb64cc";
+      #    meta = {};
+      #  })
+      #  (buildFirefoxXpiAddon {
+      #    pname = "Cookie-Editor";
+      #    version = "1.13.0";
+      #    addonId = "{c3c10168-4186-445c-9c5b-63f12b8e2c87}";
+      #    url = "https://addons.mozilla.org/firefox/downloads/file/4241002/cookie_editor-1.13.0.xpi";
+      #    sha256 = "3d6fd83a8343dfa5e4461d83c2856264fb74b36b1c165305168d013f4831dbb0";
+      #    meta = {};
+      #  })
+      #];
+    };
+    # NOTE: we need to use policies to be able to automatically pin and enable extensions
+    policies = {
+      # extensions
+      ExtensionSettings = {
+        # 1password
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # ublock
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # pwnfox
+        "PwnFox@bi.tk" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/pwnfox/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # cookie-editor
+        "{c3c10168-4186-445c-9c5b-63f12b8e2c87}" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/cookie-editor/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # sponsorblock
+        "sponsorBlocker@ajay.app" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # omnivore
+        "save-extension@omnivore.app" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/omnivore/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+        # web-scrobbler
+        "{799c0914-748b-41df-a25c-22d008f9e83f}" = {
+          installation_mode = "normal_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/web-scrobbler/latest.xpi";
+          updates_disabled = false;
+          default_area = "navbar";
+        };
+      };
     };
   };
 }
